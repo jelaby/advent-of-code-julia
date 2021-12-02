76d32d5a255cca2132bfbf90d151dc67462f0946
Paul Mealor <paul.mealor@bjss.com>
2021-12-02 09:50:45 +0000

    Day 2 part 2

--- src/day2.jl
+++ src/day2.jl
@@ -9,22 +9,22 @@ import AoC
 
 
 
-step(pos, direction, distance) = step(pos, Val(Symbol(direction)), distance)
-step(pos, ::Val{:forward},distance) = step(pos, [1,0],distance)
-step(pos, ::Val{:up}, distance) = step(pos, [0,1],distance)
-step(pos, ::Val{:down},distance) = step(pos, [0,-1],distance)
-step(pos, dir::AbstractVector, distance) = step(pos, dir * distance)
-step(pos, vec::AbstractVector) = pos + vec
 step(pos, line::AbstractString) = step(pos, split(line, r"\s+")...)
 step(pos, dir, distance::AbstractString) = step(pos, dir, parse(Int, distance))
+step(pos, direction, distance) = step(pos, Val(Symbol(direction)), distance)
+step(pos, ::Val{:forward},distance) = pos + [1,0,0]*distance + [0,1,0]*pos[3]*distance
+step(pos, ::Val{:up}, distance) = pos + [0,0,1]*distance
+step(pos, ::Val{:down},distance) = pos + [0,0,-1] * distance
 
 function exec(lines)
-    pos=[0,0]
+    pos=[0,0,0]
     for line in lines
         pos = step(pos, line)
     end
-    return pos
+    return pos[1:2]
 end
 
+show(AoC.exampleLines(2,1) |> ll -> @time exec(ll))
+show(AoC.lines(2) |> ll -> @time exec(ll))
 show(AoC.exampleLines(2,1) |> ll -> @time *(exec(ll)...))
 show(AoC.lines(2) |> ll -> @time *(exec(ll)...))

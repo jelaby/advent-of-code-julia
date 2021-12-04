467aa7a4654cf2ca96bcae5065ac361c0b569c8a
Paul Mealor <paul.mealor@bjss.com>
2021-12-04 18:34:45 +0000

    Day 4 part 2

--- src/day4.jl
+++ src/day4.jl
@@ -19,7 +19,7 @@ function createGame(lines)
     for line in 1:6:length(lines)
         card = flatten(lines[line+1:line+5] .|> strip .|> (l -> split(l, r" +")))
         card = parse.(Int, card) |> nn -> reshape(nn, 5,5)
-        @show cards = cat(cards,card; dims=3)
+        cards = cat(cards,card; dims=3)
     end
     return (numbers, cards)
 end
@@ -33,28 +33,46 @@ function bingoResult(lines)
 
     ticks = zeros(Bool, size(cards))
 
+    winningNumber = nothing
+    winningCard = nothing
+    winningTicks = nothing
+    winningCards = []
+
     for number in numbers
-        for i in eachindex(cards)
-            if cards[i] == number
-                ticks[i] = true
-            end
+        for i in 1:size(cards,3)
+                for j in 1:size(cards, 2)
+                    for k in 1:size(cards, 1)
+                        if cards[k,j,i] == number
+                            ticks[k,j,i] = true
+                        end
+                    end
+                end
         end
         for i in 1:size(cards,3)
-            for j in 1:size(cards, 2)
-                if length(filter(x->x, ticks[:,j,i])) == size(cards, 1)
-                    return calcResult(number,cards[:,:,i], ticks[:,:,i])
+            if i ∉ winningCards
+                for j in 1:size(cards, 2)
+                    if length(filter(x->x, ticks[:,j,i])) == size(cards, 1)
+                        push!(winningCards,i)
+                        winningNumber = number
+                        winningCard = deepcopy(cards[:,:,i])
+                        winningTicks = deepcopy(ticks[:,:,i])
+                    end
                 end
-            end
-            for j in 1:size(cards, 1)
-                if length(filter(x->x, ticks[j,:,i])) == size(cards, 2)
-                    return calcResult(number,cards[:,:,i], ticks[:,:,i])
+                for j in 1:size(cards, 1)
+                    if length(filter(x->x, ticks[j,:,i])) == size(cards, 2)
+                        push!(winningCards,i)
+                        winningNumber = number
+                        winningCard = deepcopy(cards[:,:,i])
+                        winningTicks = deepcopy(ticks[:,:,i])
+                    end
                 end
             end
         end
     end
+    return calcResult(winningNumber, winningCard, winningTicks)
 end
 
 
-@test bingoResult(exampleLines(4,1)) == 4512
+@test bingoResult(exampleLines(4,1)) == 1924
 
 lines(4) |> ll -> @time bingoResult(ll) |> show

badaf5a20dc378903b0e1181b2c3a6ac5f897dc8
Paul Mealor <paul.mealor@bjss.com>
2021-12-05 23:55:42 +0000

    Day 5: use a single 3-dimensional array

--- src/day5.jl
+++ src/day5.jl
@@ -6,40 +6,41 @@ day5:
 =#
 
 using AoC, Test
-using Base.Iterators: flatten
+using Base.Iterators: filter
 
-parseLine(line) = tuple( (split(line, " -> ") .|> ( part -> split(part, ",") |> x -> parse.(Int, x) )) ...)
-@test parseLine("3,5 -> 6,8") == ([3,5],[6,8])
+parseLine(line) = split(line, r",| -> ") |> cc -> parse.(Int, cc) |> cc -> reshape(cc, 2,2)
+@test parseLine("3,5 -> 6,8") == [[3, 5] [6,8]]
 
-parseLines(lines) = parseLine.(lines)
-@test parseLines(["3,5 -> 6,8", "4,6 -> 2,3"]) == [([3,5],[6,8]),([4,6],[2,3])]
+parseLines(lines) = cat((parseLine.(lines))...; dims=3)
+@test parseLines(["3,5 -> 6,8", "4,6 -> 2,3"]) == [[3,5] [6,8] ;;; [4,6] [2,3]]
 
-maxX(lines) = flatten(lines) .|> (cc -> cc[1]) |> xx -> max(xx...)
-maxY(lines) = flatten(lines) .|> (cc -> cc[2]) |> yy -> max(yy...)
+maxX(lines) = max(lines[1,:,:]...)
+maxY(lines) = max(lines[2,:,:]...)
 maxCoord(lines) = [maxX(lines), maxY(lines)]
-@test maxCoord([([1,2],[3,4]),([5,3],[1,2])]) == [5,4]
+@test maxCoord([[1,2] [3,4] ;;; [5,3] [1,2]]) == [5,4]
 
-minX(lines) = flatten(lines) .|> (cc -> cc[1]) |> xx -> min(xx...)
-minY(lines) = flatten(lines) .|> (cc -> cc[2]) |> yy -> min(yy...)
+minX(lines) = min(lines[1,:,:]...)
+minY(lines) = min(lines[2,:,:]...)
 minCoord(lines) = [minX(lines), minY(lines)]
-@test minCoord([([1,2],[0,4]),([5,3],[1,2])]) == [0,2]
+@test minCoord([[0,2] [3,4] ;;; [5,3] [1,2]]) == [0,2]
 
 mapOffset(lines) = [1,1] - minCoord(lines)
+
 mapSize(lines) = maxCoord(lines) + mapOffset(lines)
 
-direction(line) = direction(line[1],line[2])
+direction(line) = direction(line[:,1],line[:,2])
 direction(start, finish) = sign.(finish - start)
-@test direction(([1,1],[7,7])) == [1,1]
-@test direction(([1,7],[7,7])) == [1,0]
-@test direction(([7,1],[1,7])) == [-1,1]
+@test direction([[1,1] [7,7]]) == [1,1]
+@test direction([[1,7] [7,7]]) == [1,0]
+@test direction([[7,1] [1,7]]) == [-1,1]
 
 function createMap(lines)
     map = zeros(Int, mapSize(lines)...)
     offset = mapOffset(lines)
 
-    for line in lines
-        point = line[1] + offset
-        endPoint = line[2] + offset
+    for line in eachslice(lines; dims=3)
+        point = line[:,1] + offset
+        endPoint = line[:,2] + offset
         dir = direction(line)
         while true
             map[point...]+=1
@@ -53,11 +54,11 @@ function createMap(lines)
     return map
 end
 
-selectPart1(lines) = filter(lines) do line
-    (start, finish) = line
+selectPart1(lines) = cat(filter(eachslice(lines, dims=3)) do line
+    (start, finish) = (line[:,1], line[:,2])
     return start[1] == finish[1] || start[2] == finish[2]
-end
-@test selectPart1([((2,2),(4,4)), ((2,2),(2,5))]) == [((2,2),(2,5))]
+end...; dims=3)
+@test selectPart1([[2,2] [4,4] ;;; [2,2] [2,5]]) == [[2,2] [2,5] ;;;]
 
 part1(lines) = parseLines(lines) |> selectPart1 |> createMap |> map -> count(x -> x>=2, map)
 part2(lines) = parseLines(lines) |> createMap |> map -> count(x -> x>=2, map)

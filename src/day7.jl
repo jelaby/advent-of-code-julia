19130a720f8ff348857a0307f9488291be016ce3
Paul Mealor <paul.mealor@bjss.com>
2021-12-07 09:46:26 +0000

    Day 7 part 2

--- src/day7.jl
+++ src/day7.jl
@@ -17,6 +17,11 @@ totalDistance(candidate, positions) = sum(p -> abs(candidate - p), positions)
 @test totalDistance(3,[16,1,2,0,4,2,7,1,2,14]) == 39
 @test totalDistance(10,[16,1,2,0,4,2,7,1,2,14]) == 71
 
+totalDistance2(candidate, positions) = sum(p -> sum(1:abs(candidate - p)), positions)
+@test totalDistance2(4,[4,4]) == 0
+@test totalDistance2(5,[16,1,2,0,4,2,7,1,2,14]) == 168
+@test totalDistance2(2,[16,1,2,0,4,2,7,1,2,14]) == 206
+
 function bestPosition(positions)
     bestDistance = typemax(eltype(positions))
     result = -1
@@ -30,7 +35,21 @@ function bestPosition(positions)
     return result
 end
 
+function bestPosition2(positions)
+    bestDistance = typemax(eltype(positions))
+    result = -1
+    for candidate = min(positions...):max(positions...)
+        thisDistance = totalDistance2(candidate, positions)
+        if thisDistance < bestDistance
+            bestDistance = thisDistance
+            result = candidate
+        end
+    end
+    return result
+end
+
 @test bestPosition([16,1,2,0,4,2,7,1,2,14]) == 2
+@test bestPosition2([16,1,2,0,4,2,7,1,2,14]) == 5
 
 function part1(line)
     positions = parsePositions(line)
@@ -40,4 +59,13 @@ end
 
 @test part1(firstExampleLine(7,1)) == 37
 
+function part2(line)
+    positions = parsePositions(line)
+    position = bestPosition2(positions)
+    return totalDistance2(position, positions)
+end
+
+@test part2(firstExampleLine(7,1)) == 168
+
 firstLine(7) |> l -> @time part1(l) |> show
+firstLine(7) |> l -> @time part2(l) |> show

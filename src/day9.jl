2a1ab4bd227dc2721cb4911fe717b283e52de0a3
Paul Mealor <paul.mealor@bjss.com>
2021-12-09 23:26:26 +0000

    Day 9 part 2

--- src/day9.jl
+++ src/day9.jl
@@ -32,7 +32,34 @@ lowpointHeights(caveMap) = caveMap[lowpointLocations(caveMap)]
 @test lowpointHeights(parseCaveMap(exampleLines(9,1))) == [1,0,5,5]
 
 part1(lines) = parseCaveMap(lines) |> lowpointHeights |> hh -> sum(h+1 for h in hh)
-
 @test part1(exampleLines(9,1)) == 15
 
+function findBasin(A, I, basin=Set())
+    if I ∈ basin
+        return basin
+    else
+        basin = union(basin, [I])
+        for J in [I+n for n in NEIGHBOURS]
+            if J ∉ basin && checkbounds(Bool, A, J) && A[J] < 9 && A[J] > A[I]
+                basin = union(basin, findBasin(A, J, basin))
+            end
+        end
+        return basin
+    end
+end
+
+basinSize(A, I) = length(findBasin(A, I))
+@test basinSize(exampleLines(9,1) |> parseCaveMap, CartesianIndex((2,1))) == 3
+@test basinSize(exampleLines(9,1) |> parseCaveMap, CartesianIndex((10,1))) == 9
+@test basinSize(exampleLines(9,1) |> parseCaveMap, CartesianIndex((3,3))) == 14
+@test basinSize(exampleLines(9,1) |> parseCaveMap, CartesianIndex((7,5))) == 9
+
+function part2(lines)
+    caveMap = parseCaveMap(lines)
+    basinSizes = [basinSize(caveMap, I) for I in lowpointLocations(caveMap)]
+    return *(sort(basinSizes; lt = >)[1:3]...)
+end
+@test part2(exampleLines(9,1)) == 1134
+
 lines(9) |> ll -> @time part1(ll) |> show
+lines(9) |> ll -> @time part2(ll) |> show

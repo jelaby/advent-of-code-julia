93a5fb919aa78609744ba2f79f4bc57fd398ac2a
Paul Mealor <paul.mealor@bjss.com>
2021-12-12 10:48:20 +0000

    Day 12 part 2

--- src/day12.jl
+++ src/day12.jl
@@ -5,13 +5,13 @@ day12:
 - Date: 2021-12-12
 =#
 
-using AoC, Test
+using AoC, Test, Memoize
 
 
 function parseLink!(links, link)
     (start,finish) = split(link, "-")
-    links[start] = union(get(links,start,[]), [finish])
-    links[finish] = union(get(links,finish,[]), [start])
+    if finish != "start"; links[start] = union(get(links,start,[]), [finish]); end
+    if start != "start"; links[finish] = union(get(links,finish,[]), [start]); end
     return links
 end
 @test parseLink!(Dict(), "a-b") == Dict("a"=>["b"], "b"=>["a"])
@@ -25,30 +25,49 @@ function parseLinks(links)
     return result
 end
 
-function visit!(paths, visited, links, cave)
-    visited = vcat(visited, cave)
+@memoize manyVisitsAllowed(cave) = occursin(r"^[A-Z]+$", cave)
+
+visit(links, cave; allowTwoVisits=false) = visit!(links, cave, Set(), []; allowTwoVisits)
+function visit!(links, cave, paths, visited; allowTwoVisits=false)
+    push!(visited, cave)
     if cave == "end"
-        push!(paths, visited)
+        if visited ∉ paths
+            push!(paths, copy(visited))
+        end
+        pop!(visited)
         return paths
     end
 
     for target in links[cave]
-        if occursin(r"^[A-Z]+$", target) || target ∉ visited
-            visit!(paths, visited, links, target)
+        if manyVisitsAllowed(target)
+            paths = visit!(links, target, paths, visited; allowTwoVisits=allowTwoVisits)
+        elseif allowTwoVisits
+            paths = visit!(links, target, paths, visited; allowTwoVisits=(target ∉ visited))
+        elseif target ∉ visited
+            paths = visit!(links, target, paths, visited; allowTwoVisits=allowTwoVisits)
         end
     end
 
+    pop!(visited)
     return paths
 end
-@test visit!([], [], Dict("start"=>["end"], "end"=>["start"]), "start") == [["start","end"],]
+@test visit(Dict("start"=>["end"]), "start") == Set([["start", "end"],])
 
 function part1(lines)
     links = parseLinks(lines)
-    paths = visit!([], [], links, "start")
-    return length(paths)
+    return visit(links, "start") |> length
 end
 @test part1(exampleLines(12,1)) == 10
 @test part1(exampleLines(12,2)) == 19
 @test part1(exampleLines(12,3)) == 226
 
+function part2(lines)
+    links = parseLinks(lines)
+    return visit(links, "start"; allowTwoVisits=true) |> length
+end
+@test part2(exampleLines(12,1)) == 36
+@test part2(exampleLines(12,2)) == 103
+@test part2(exampleLines(12,3)) == 3509
+
 lines(12) |> ll -> @time part1(ll) |> show
+lines(12) |> ll -> @time part2(ll) |> show

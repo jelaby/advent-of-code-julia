f52e472fe371bc92ea042f37fb0d29972a0ca8ab
Paul Mealor <paul.mealor@bjss.com>
2021-12-14 10:47:37 +0000

    Day 14 part 2

--- src/day14.jl
+++ src/day14.jl
@@ -9,84 +9,68 @@ using AoC, Test
 using StructEquality # https://docs.juliahub.com/StructEquality/TwsrV/1.0.0/
 
 
-struct Rule
-    left::Char
-    right::Char
-    middle::Char
+@def_structequal struct Rule{T}
+    target::T
+    results::Vector{T}
 end
 
-parseRule(line) = Rule(getindex.(match(r"^(.)(.) -> (.)", line).captures, 1)...)
-@test parseRule("AB -> C") == Rule('A','B','C')
+function parseRule(line)
+    captures = match(r"^((.)(.)) -> (.)", line).captures
+    return Rule(String(captures[1]),[captures[2]*captures[4],captures[4]*captures[3]])
+end
+@test parseRule("AB -> C") == Rule("AB",["AC","CB"])
 
-parseInput(lines) = ([c for c in lines[1]], parseRule.(lines[3:end]))
-@test parseInput(["ABCD","","AB -> C", "AC -> D"]) == (['A','B','C','D'], [Rule('A','B','C'),Rule('A','C','D')])
-
-@def_structequal mutable struct Node{T}
-    value::T
-    next::Union{Node{T},Nothing}
-end
-function Node(v::Vector{T}) where T
-    node = nothing
-    for c in v[end:-1:1]
-        node = Node(c,node)
-    end
-    return node
-end
-@test Node(['a','b']) == Node{Char}('a',Node{Char}('b',nothing))
-function insert!(node::Node{T}, e::T) where T
-    e = Node(e,node.next)
-    node.next = e
-end
-function toVector(node::Node{T}) where T
-    result = T[]
-    while !isnothing(node)
-        push!(result, node.value)
-        node = node.next
+function toPairs(line::T) where T
+    result = Dict{T,Int}()
+    for i in 1:length(line)-1
+        pair = line[i:i+1]
+        result[pair] = get(result,pair,0) + 1
     end
     return result
 end
-@test toVector(Node('a',Node('b',nothing))) == ['a','b']
+@test toPairs("ABCD") == Dict("AB"=>1,"BC"=>1,"CD"=>1)
 
-Step(input::Tuple) = doStep(input...)
-doStep(state::Vector{T}, rules::Vector{Rule}) where T = doStep(Node(state), rules) |> toVector
-function doStep(state::Node{T}, rules::Vector{Rule}) where T
-    result = state
-    node = state
+parseInput(lines) = (toPairs(lines[1]), parseRule.(lines[3:end]))
+@test parseInput(["ABCD","","AB -> C", "AC -> D"]) == (Dict("AB"=>1,"BC"=>1,"CD"=>1), [Rule("AB",["AC","CB"]),Rule("AC",["AD","DC"])])
 
-    while !isnothing(node.next)
-        left = node.value
-        nextNode = node.next
-        right = nextNode.value
-        for rule in rules
-            if left == rule.left && right == rule.right
-                insert!(node, rule.middle)
+
+doStep(input::Tuple) = doStep(input...)
+function doStep(state, rules)
+    newState = copy(state)
+    for rule in rules
+        elementCount = get(state,rule.target,0)
+        if elementCount != 0
+            newState[rule.target] -= elementCount
+            if newState[rule.target] == 0
+                delete!(newState, rule.target)
+            end
+            for sequence in rule.results
+                newState[sequence] = get(newState,sequence,0) + elementCount
             end
         end
-        node = nextNode
     end
-    return result
+    return newState
 end
-@test doStep(parseInput(exampleLines(14,1))) == ['N','C','N','B','C','H','B']
+@test doStep(parseInput(exampleLines(14,1))) == Dict("NC"=>1,"CN"=>1,"NB"=>1,"BC"=>1,"CH"=>1,"HB"=>1)
 
 doSteps(input::Tuple, n) = doSteps(input..., n)
-doSteps(state::Vector{T}, rules::Vector{Rule}, n) where T = doSteps(Node(state), rules, n)
 function doSteps(state, rules, n)
     for i in 1:n
         state = doStep(state, rules)
     end
     return state
 end
-@test doSteps(parseInput(exampleLines(14,1)),1) |> toVector == [c for c in "NCNBCHB"]
-@test doSteps(parseInput(exampleLines(14,1)),2) |> toVector == [c for c in "NBCCNBBBCBHCB"]
-@test doSteps(parseInput(exampleLines(14,1)),3) |> toVector == [c for c in "NBBBCNCCNBBNBNBBCHBHHBCHB"]
-@test doSteps(parseInput(exampleLines(14,1)),4) |> toVector == [c for c in "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"]
+@test doSteps(parseInput(exampleLines(14,1)),1) == toPairs("NCNBCHB")
+@test doSteps(parseInput(exampleLines(14,1)),2) == toPairs("NBCCNBBBCBHCB")
+@test doSteps(parseInput(exampleLines(14,1)),3) == toPairs("NBBBCNCCNBBNBNBBCHBHHBCHB")
+@test doSteps(parseInput(exampleLines(14,1)),4) == toPairs("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")
 
 function answer(lines, steps)
+    lastElement = lines[1][end]
     state = doSteps(parseInput(lines), steps)
-    counts = Dict{Char, Int}()
-    while state != nothing
-        counts[state.value] = get(counts,state.value,0) + 1
-        state = state.next
+    counts = Dict{Char, Int}(lastElement=>1)
+    for (pair,count) in state
+        counts[pair[1]] = get(counts,pair[1],0)+count
     end
     mostElementCount = argmax(pair->pair[2], counts)[2]
     leastElementCount = argmin(pair->pair[2], counts)[2]

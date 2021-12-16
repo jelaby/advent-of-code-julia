ddfde317e3a0c5531735f5365289de43d58a0a7a
Paul Mealor <paul.mealor@bjss.com>
2021-12-16 14:34:53 +0000

    Day 16: part 2

--- src/day16.jl
+++ src/day16.jl
@@ -23,6 +23,9 @@ function read!(stream::BitStream{T}, n)::UInt where T
     stream
     result::UInt = 0
     while n > 0
+        if (stream.byteOffset > length(stream.values))
+            throw(BoundsError(stream.values, stream.byteOffset))
+        end
         remainingBitsInByte = elbits - stream.bitOffset
         bitsToRead = min(remainingBitsInByte, n)
 
@@ -51,10 +54,13 @@ testStream = BitStream(UInt8[0b10101110, 0b00100000, 0b00000000, 0b00000000])
 @test read!(testStream, 5) == 0b10001
 
 
+function eof(stream::BitStream)
+    return stream.byteOffset > length(stream.values)
+end
 
 function lineToStream(line)
-    @show charsPerWord = sizeof(UInt) * 2
-    @show length(line), length(line) % charsPerWord, line
+    charsPerWord = sizeof(UInt) * 2
+    length(line), length(line) % charsPerWord, line
     if length(line) % charsPerWord != 0
         line = rpad(line, length(line) + charsPerWord - (length(line) % charsPerWord), '0')
     end
@@ -71,6 +77,7 @@ end
 
 struct OperatorPacket <: AbstractPacket
     version::UInt
+    operator
     packets::Vector{AbstractPacket}
 end
 
@@ -89,11 +96,11 @@ function readLiteralValue!(stream)
     return (result, bitsRead)
 end
 
-function readPackets!(stream; maxLength=typemax(UInt), maxPackets=typemax(UInt))
+function readPackets!(stream; maxLength=typemax(Int), maxPackets=typemax(Int))
     bitsRead = 0
     packetsRead = 0
     packets = AbstractPacket[]
-    while bitsRead < maxLength && packetsRead < maxPackets
+    while bitsRead < maxLength && packetsRead < maxPackets && !eof(stream)
         (packet, bitsReadThisTime) = readPacket!(stream)
         push!(packets, packet)
         bitsRead += bitsReadThisTime
@@ -102,34 +109,60 @@ function readPackets!(stream; maxLength=typemax(UInt), maxPackets=typemax(UInt))
     return (packets, bitsRead)
 end
 
+sumOperator(values) = +(0, values...)
+productOperator(values) = *(1, values...)
+minimumOperator(values) = min(values...)
+maximumOperator(values) = max(values...)
+greaterThanOperator(values) = values[1] > values[2]
+lessThanOperator(values) = values[1] < values[2]
+equalToOperator(values) = values[1] == values[2]
+
 function readPacket!(stream)
     version = read!(stream, 3)
     typeId = read!(stream, 3)
     bitsRead = 6
 
     if typeId == 4
-        @show (value, bitsReadThisTime) = readLiteralValue!(stream)
+        (value, bitsReadThisTime) = readLiteralValue!(stream)
         bitsRead += bitsReadThisTime
         return (ValuePacket(version, value), bitsRead)
     else
         # operator packet
         lengthTypeId = read!(stream, 1)
+        bitsRead += 1
         if lengthTypeId == 0
             contentLength = read!(stream, 15)
-            @show (packets, bitsReadThisTime) = readPackets!(stream; maxLength = contentLength)
-            bitsRead += bitsReadThisTime
-            return (OperatorPacket(version, packets), bitsRead)
+            bitsRead += 15
+            (packets, bitsReadThisTime) = readPackets!(stream; maxLength = contentLength)
         else
             packetCount = read!(stream, 11)
-            @show (packets, bitsReadThisTime) = readPackets!(stream; maxPackets = packetCount)
-            bitsRead += bitsReadThisTime
-            return (OperatorPacket(version, packets), bitsRead)
+            bitsRead += 11
+            (packets, bitsReadThisTime) = readPackets!(stream; maxPackets = packetCount)
         end
+        bitsRead += bitsReadThisTime
+
+        if typeId == 0
+            operator = sumOperator
+        elseif typeId == 1
+            operator = productOperator
+        elseif typeId == 2
+            operator = minimumOperator
+        elseif typeId == 3
+            operator = maximumOperator
+        elseif typeId == 5
+            operator = greaterThanOperator
+        elseif typeId == 6
+            operator = lessThanOperator
+        elseif typeId == 7
+            operator = equalToOperator
+        end
+
+        return (OperatorPacket(version, operator, packets), bitsRead)
     end
 end
 
 totalValue(packet::ValuePacket) = packet.value
-totalValue(packet::OperatorPacket) = +(totalValue.(packet.packets)...)
+totalValue(packet::OperatorPacket) = packet.operator(totalValue.(packet.packets))
 
 totalVersions(packet::ValuePacket) = packet.version
 totalVersions(packet::OperatorPacket) = packet.version +(totalVersions.(packet.packets)...)
@@ -137,9 +170,13 @@ totalVersions(packet::OperatorPacket) = packet.version +(totalVersions.(packet.p
 function part1(line)
     (packet, bitsRead) = readPacket!(lineToStream(line))
 
-    @show packet
-
     return signed(totalVersions(packet))
+end
+
+function part2(line)
+    (packet, bitsRead) = readPacket!(lineToStream(line))
+
+    return signed(totalValue(packet))
 end
 
 @test part1("D2FE28") == 6
@@ -148,4 +185,14 @@ end
 @test part1("C0015000016115A2E0802F182340") == 23
 @test part1("A0016C880162017C3686B18A3D4780") == 31
 
+@test part2("C200B40A82") == 3
+@test part2("04005AC33890") == 54
+@test part2("880086C3E88112") == 7
+@test part2("CE00C43D881120") == 9
+@test part2("D8005AC2A8F0") == 1
+@test part2("F600BC2D8F") == 0
+@test part2("9C005AC2F8F0") == 0
+@test part2("9C0141080250320F1802104A08") == 1
+
 @show lines(16)[1] |> l -> @time part1(l)
+@show lines(16)[1] |> l -> @time part2(l)

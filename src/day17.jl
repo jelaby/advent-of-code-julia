75f24cbbebd2844042d99069760cf9131ada4fef
Paul Mealor <paul.mealor@bjss.com>
2021-12-17 13:34:51 +0000

    Day 17 part 2

--- src/day17.jl
+++ src/day17.jl
@@ -7,17 +7,14 @@ day17:
 
 using Test, AoC
 
-
-
-
 function launch(v, target)
     p = [0,0]
     maxHeight = p[2]
-    while p[1] <= target[1,2] && p[2] >= target[2,1]
+    while (p[1] <= target[1,2]) && (p[2] >= target[2,1])
         if p[2] > maxHeight
             maxHeight = p[2]
         end
-        if target[1,1] <= p[1] <= target[1,2] && target[2,1] <= p[2] <= target[2,1]
+        if (target[1,1] <= p[1] <= target[1,2]) && (target[2,1] <= p[2] <= target[2,2])
             return maxHeight
         end
         p = p + v
@@ -26,28 +23,46 @@ function launch(v, target)
     end
     return false
 end
-#@test launch([17,-4], [[20, -10] [30, -5]]) == false
-#@test launch([7,2], [[20, -10] [30, -5]]) == 3
-#@test launch([6,3], [[20, -10] [30, -5]]) == 6
+@test launch([17,-4], [[20, -10] [30, -5]]) == false
+@test launch([7,2], [[20, -10] [30, -5]]) == 3
+@test launch([6,3], [[20, -10] [30, -5]]) == 6
 @test launch([6,9], [[20, -10] [30, -5]]) == 45
+@test launch([23,-10], [[20, -10] [30, -5]]) == 0
+
+exampleLines(17,1) .|> line -> @test launch(parse.(Int, split(line, ",")), [[20, -10] [30, -5]]) !== false
 
 function part1(target)
     maxHeight = 0
     for v_x ∈ 1:target[1,2]
-        for v_y ∈ 1:-target[2,1]-1
+        for v_y ∈ 1:abs(target[2,1])+1
             height = launch([v_x,v_y], target)
             if height > maxHeight
-                @show maxHeight = height
+                maxHeight = height
             end
         end
     end
     return maxHeight
 end
 
+function part2(target)
+    hits = 0
+    for v_x ∈ 1:target[1,2]
+        for v_y ∈ -abs(target[2,1])-1:abs(target[2,1])+1
+            height = launch([v_x,v_y], target)
+            if height !== false
+                hits += 1
+            end
+        end
+    end
+    return hits
+end
+
 
 
 @test part1([[20, -10] [30, -5]]) == 45
+@test part2([[20, -10] [30, -5]]) == 112
 
 
 
-@show part1([[96, -144] [125,-98]])
+@show @time part1([[96, -144] [125,-98]])
+@show @time part2([[96, -144] [125,-98]])

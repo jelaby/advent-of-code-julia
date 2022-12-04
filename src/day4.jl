c89a330ac795a6637fcdfb8360030cdd922b086c
Paul Mealor <paul.mealor@bjss.com>
2022-12-04 09:32:19 +0000

    Day 4 part 2

--- src/day4.jl
+++ src/day4.jl
@@ -19,4 +19,13 @@ end
 @test part1(["6-6,4-6"]) == 1
 @test part1(AoC.exampleLines(4,1)) == 2
 
-AoC.day(4, part1)
+part2(lines) = count(split.(lines, r"[,-]")) do areas
+    areas = parse.(Int, areas)
+    (areas[1] <= areas[3] && areas[2] >= areas[3]) || (areas[3] <= areas[1] && areas[4] >= areas[1])
+end
+@test part2(["2-4,6-8"]) == 0
+@test part2(["5-7,7-9"]) == 1
+@test part2(["6-6,4-6"]) == 1
+@test part2(AoC.exampleLines(4,1)) == 4
+
+AoC.day(4, part1, part2)

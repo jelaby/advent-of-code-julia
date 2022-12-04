1a8d373b25a3ee9d44eda23e9c137773301950fa
Paul Mealor <paul.mealor@bjss.com>
2022-12-04 09:49:36 +0000

    Day 4 tidying up

--- src/day4.jl
+++ src/day4.jl
@@ -4,28 +4,28 @@ day3:
 - Author: Paul.Mealor
 - Date: 2022-12-04
 =#
-include("AoC.jl")
-
 using Test
 
+lines = open(readlines, "src/day4-input.txt")
+example1 = open(readlines, "src/day4-example-1.txt")
 
-
-part1(lines) = count(split.(lines, r"[,-]")) do areas
-    areas = parse.(Int, areas)
-    (areas[1] <= areas[3] && areas[2] >= areas[4]) || (areas[3] <= areas[1] && areas[4] >= areas[2])
+part1(lines) = count(lines) do sections
+    sections = parse.(Int, split(sections, r"[,-]"))
+    (sections[1] <= sections[3] && sections[2] >= sections[4]) || (sections[3] <= sections[1] && sections[4] >= sections[2])
 end
 @test part1(["2-4,6-8"]) == 0
 @test part1(["5-7,7-9"]) == 0
 @test part1(["6-6,4-6"]) == 1
-@test part1(AoC.exampleLines(4,1)) == 2
+@test part1(example1) == 2
 
-part2(lines) = count(split.(lines, r"[,-]")) do areas
-    areas = parse.(Int, areas)
-    (areas[1] <= areas[3] && areas[2] >= areas[3]) || (areas[3] <= areas[1] && areas[4] >= areas[1])
+part2(lines) = count(lines) do sections
+    sections = parse.(Int, split(sections, r"[,-]"))
+    sections[1] <= sections[3] <= sections[2] || sections[3] <= sections[1] <= sections[4]
 end
 @test part2(["2-4,6-8"]) == 0
 @test part2(["5-7,7-9"]) == 1
 @test part2(["6-6,4-6"]) == 1
-@test part2(AoC.exampleLines(4,1)) == 4
+@test part2(example1) == 4
 
-AoC.day(4, part1, part2)
+show(@time part1(lines))
+show(@time part2(lines))

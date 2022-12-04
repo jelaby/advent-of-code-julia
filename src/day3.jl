0e900eb4dcd73f14326471f60443e37bad412002
Paul Mealor <paul.mealor@bjss.com>
2022-12-04 10:46:38 +0000

    Day 3: remove overengineering born of misreading the question

--- src/day3.jl
+++ src/day3.jl
@@ -9,9 +9,10 @@ include("AoC.jl")
 using Test
 using Base.Iterators
 
+charSet(s) = Set([c for c in s])
 
-findDuplicate(l, r) = findDuplicate(Set([c for c in l]), r)
-findDuplicate(l::Set, r) = r[findfirst(item -> item ∈ l, r)]
+findDuplicate(l, r) = findDuplicate(charSet(l), r)
+findDuplicate(l::Set, r) = first(l ∩ r)
 findDuplicate(spec) = findDuplicate(SubString(spec, 1, length(spec)÷2), SubString(spec, 1+length(spec)÷2))
 @test findDuplicate("abcd", "defg") == 'd'
 @test findDuplicate("ABcd", "abcD") == 'c'
@@ -23,24 +24,8 @@ priority(i) = 'a' <= i <= 'z' ? 1 + (i - 'a') : 27 + (i - 'A')
 @test priority('A') == 27
 @test priority('Z') == 52
 
-findDuplicates(lines) = findDuplicate.(lines)
-
-part1(lines) = sum(priority.(findDuplicates(lines)))
-
+part1(lines) = sum(priority.(findDuplicate.(lines)))
 @test part1(AoC.exampleLines(3,1)) == 157
-
-
-findCounts(lines::Vector{<:AbstractString}) = findCounts(flatten(lines .|> line -> Set([c for c in line])))
-function findCounts(items)
-    T = eltype(items)
-    result = Dict{T, Int}()
-    for i in items
-        push!(result, i => get(result, i, 0) + 1)
-    end
-    return result
-end
-@test findCounts(["abcd", "cdef"]) == Dict('a' => 1, 'b' => 1, 'c' => 2, 'd' => 2, 'e' => 1, 'f' => 1)
-@test findCounts(["abcb", "cdef"]) == Dict('a' => 1, 'b' => 1, 'c' => 2, 'd' => 1, 'e' => 1, 'f' => 1)
 
 function findIdCards(lines)
     result = []
@@ -50,16 +35,10 @@ function findIdCards(lines)
     return result
 end
 
-
-findGroupIdCards(lines) = keys(filter(findCounts(lines)) do count
-    count.second == 3
-end)
-
+findGroupIdCards(lines) = first(reduce(∩, charSet.(lines)))
 @test findIdCards(AoC.exampleLines(3,1)) == ['r', 'Z']
 
-
 part2(lines) = sum(priority.(findIdCards(lines)))
-
 @test part2(AoC.exampleLines(3,1)) == 70
 
 AoC.day(3, part1, part2)

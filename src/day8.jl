dab87010b82462063a21af3128a34043621a39c5
Paul Mealor <paul.mealor@bjss.com>
2022-12-08 11:12:47 +0000

    Day 8 part 2

--- src/day8.jl
+++ src/day8.jl
@@ -9,6 +9,7 @@ using Test
 lines = open(readlines, "src/day8-input.txt")
 example1 = open(readlines, "src/day8-example-1.txt")
 
+heights(lines) = reshape([parse(Int, c) for line in lines for c in line], length(lines[1]), length(lines))
 
 findVisible(heights) = count(CartesianIndices(heights)) do I
     I[1] == 1 || I[1] == size(heights, 1) ||
@@ -19,9 +20,43 @@ findVisible(heights) = count(CartesianIndices(heights)) do I
     heights[I] > reduce(max, heights[I[1],I[2]+1:end])
 end
 
-part1(lines) = findVisible(reshape([parse(Int, c) for line in lines for c in line], length(lines[1]), length(lines)))
+scenicScore(heights, I, direction::Tuple) = scenicScore(heights, I, CartesianIndex(direction))
+function scenicScore(heights, I, direction)
+    height = heights[I]
+    J = I
+    score = 0
+    while true
+        J = J + direction
+        if !checkbounds(Bool, heights, J)
+            return score
+        end
+        score += 1
+        if heights[J] >= height
+            return score
+        end
+    end
+end
+@test scenicScore(heights(example1), CartesianIndex((3,2)), (1,0)) == 2
+@test scenicScore(heights(example1), CartesianIndex((3,2)), (-1,0)) == 1
+@test scenicScore(heights(example1), CartesianIndex((3,2)), (0,1)) == 2
+@test scenicScore(heights(example1), CartesianIndex((3,2)), (0,-1)) == 1
+@test scenicScore(heights(example1), CartesianIndex((3,4)), (1,0)) == 2
+@test scenicScore(heights(example1), CartesianIndex((3,4)), (-1,0)) == 2
+@test scenicScore(heights(example1), CartesianIndex((3,4)), (0,1)) == 1
+@test scenicScore(heights(example1), CartesianIndex((3,4)), (0,-1)) == 2
+
+findBestTree(heights) = maximum(CartesianIndices(heights)) do I
+    scenicScore(heights, I, (1,0)) *
+    scenicScore(heights, I, (-1,0)) *
+    scenicScore(heights, I, (0,1)) *
+    scenicScore(heights, I, (0,-1))
+end
+
+part1 = findVisible ∘ heights
+part2 = findBestTree ∘ heights
 
 @test part1(example1) == 21
+@test part2(example1) == 8
 
 show(@time part1(lines))
-#show(@time part2(lines))
+show(@time part2(lines))

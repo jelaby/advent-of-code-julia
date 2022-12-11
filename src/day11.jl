e8021344f19998b45ad9ebc7a1ff1aae5fb72f9d
Paul Mealor <paul.mealor@bjss.com>
2022-12-11 23:45:35 +0000

    Day 11 part 1

---
+++ src/day11.jl
@@ -0,0 +1,88 @@
+#=
+day11:
+- Julia version: 1.8.3
+- Author: Paul.Mealor
+- Date: 2022-12-11
+=#
+using Test
+
+lines = open(readlines, "src/day11-input.txt")
+example1 = open(readlines, "src/day11-example-1.txt")
+
+struct Operation
+    operator
+    getter
+end
+
+Operation(operator::AbstractString, getter::AbstractString) =
+    Operation(operatorFor(Val(Symbol(operator))), getterFor(getter))
+
+mutable struct Monkey
+    const number::Int
+    const items::Vector{Int}
+    const operation::Operation
+    const factor::Int
+    const trueTarget::Int
+    const falseTarget::Int
+    inspectionCount::Int
+end
+Monkey(number, items, operation, factor, trueTarget, falseTarget) = Monkey(number, items, operation, factor, trueTarget, falseTarget, 0)
+
+operatorFor(::Val{:*}) = (a,b) -> a * b
+operatorFor(::Val{:+}) = (a,b) -> a + b
+
+function getterFor(getter)
+    if getter == "old"
+        return worry -> worry
+    else
+        value = parse(Int, getter)
+        return worry -> value
+    end
+end
+
+parseMonkey(lines) =
+    Monkey(
+        match(r"Monkey (\d+):", lines[1]).captures[1] |> x -> parse(Int, x),
+        match(r"Starting items: ((?:\d+)(?:, *\d+)*)", lines[2]).captures[1] |> x -> split(x, r", *") |> x -> parse.(Int, x),
+        Operation(match(r"Operation: new = old ([+*]) (\d+|old)", lines[3]).captures...),
+        match(r"Test: divisible by (\d+)", lines[4]).captures[1] |> x -> parse(Int, x),
+        match(r"If true: throw to monkey (\d+)", lines[5]).captures[1] |> x -> parse(Int, x),
+        match(r"If false: throw to monkey (\d+)", lines[6]).captures[1] |> x -> parse(Int, x)
+    )
+
+parseMonkeys(lines) = [parseMonkey(lines[l:l+5]) for l in 1:7:length(lines)]
+
+function turn(monkey, monkeys)
+    for item in monkey.items
+        item = monkey.operation.operator(item, monkey.operation.getter(item)) ÷ 3
+        monkey.inspectionCount += 1
+        if (item % monkey.factor) == 0
+            push!(monkeys[monkey.trueTarget+1].items, item)
+        else
+            push!(monkeys[monkey.falseTarget+1].items, item)
+        end
+    end
+    empty!(monkey.items)
+end
+
+function round(monkeys)
+    for monkey in monkeys
+        turn(monkey, monkeys)
+    end
+end
+
+function rounds(monkeys, number)
+    for i in 1:number
+        round(monkeys)
+    end
+    return @show monkeys
+end
+
+part1(lines) = parseMonkeys(lines) |>
+    monkeys -> rounds(monkeys, 20) |>
+    monkeys -> sort!(monkeys, by=monkey -> monkey.inspectionCount, rev=true) |>
+    monkeys -> monkeys[1].inspectionCount * monkeys[2].inspectionCount
+
+@test part1(example1) == 10605
+
+@time println(part1(lines))

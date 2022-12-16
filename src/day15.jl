0b6965eb5551f4f5301480cfdad5915595e6bbaa
Paul Mealor <paul.mealor@bjss.com>
2022-12-16 00:32:56 +0000

    Day 15: slight speed increase on part 1

--- src/day15.jl
+++ src/day15.jl
@@ -23,6 +23,70 @@ parseSensor(line) = match(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at
     numbers -> parse.(Int, numbers) |>
     numbers -> Sensor(numbers...)
 
+struct UnitRanges
+    a::Union{UnitRanges,UnitRange}
+    b::UnitRange
+end
+UnitRanges(a::UnitRange, b::UnitRanges) = UnitRanges(b,a)
+UnitRanges(a::UnitRanges, b::UnitRanges) = UnitRanges(UnitRanges(UnitRanges(a.a,a.b),b.a),b.b)
+
+function Base.union(a::UnitRange, b::UnitRanges)
+    aa = a ∪ b.a
+    ab = a ∪ b.b
+
+    if aa isa UnitRange
+        return aa ∪ b.b
+    elseif ab isa UnitRange
+        return ab ∪ b.a
+    else
+        return UnitRanges(aa,b)
+    end
+end
+Base.union(a::UnitRanges, b::UnitRange) = union(b,a)
+
+Base.union(a::UnitRanges, b::UnitRanges) = a.a ∪ b ∪ a.b
+
+
+function Base.union(a::UnitRange, b::UnitRange)
+    if a.start <= b.start && a.stop >= b.stop
+        return a;
+    elseif b.start <= a.start && b.stop >= a.stop
+        return b;
+    elseif (a.start <= b.start && a.stop >= b.stop) ||
+        (b.start <= a.start && b.stop >= a.stop) ||
+        (a.stop + 1 == b.start) ||
+        (b.stop + 1 == a.start)
+        return min(a.start,b.start):max(a.stop,b.stop)
+    else
+        return UnitRanges(a,b)
+    end
+end
+@test union(1:3,4:6) == 1:6
+@test union(1:3,5:6) == UnitRanges(1:3,5:6)
+@test (1:3) ∪ (5:6) ∪ (4:4) == 1:6
+
+function Base.setdiff(a::UnitRange, b::UnitRange)
+    if a.start <= b.start && a.stop >= b.stop
+        return UnitRanges(a.start:b.start-1, b.stop+1:a.stop)
+    elseif b.start <= a.start && b.stop >= a.stop
+        return Nothing
+    elseif a.start <= b.start && a.stop >= b.start
+        return a.start:b.start-1
+    elseif b.start <= a.start && b.stop >= a.start
+        return b.stop+1:a.stop
+    else
+        return a
+    end
+end
+
+function Base.setdiff(a::UnitRange, b::UnitRanges)
+    return setdiff(setdiff(a,b.a),b.b)
+end
+
+function Base.setdiff(a::UnitRanges, b::UnitRanges)
+    return setdiff(a.a,b) ∪ setdiff(a.b,b)
+end
+
 #=
    #
   ###
@@ -45,18 +109,37 @@ end
 beaconsOnRow(sensors, row) = filter([s.beacon for s in sensors]) do beacon
     beacon[2] == row
 end
-beaconPositionsOnRow(sensors, row) = [beacon[1] for beacon in beaconsOnRow(sensors, row)]
-@test beaconPositionsOnRow([Sensor(0,0,0,0), Sensor(0,0,14,1), Sensor(0,0,17,0)], 0) == [0,17]
-@test beaconPositionsOnRow([Sensor(0,0,0,0), Sensor(0,0,14,1), Sensor(0,0,17,0)], 1) == [14]
+beaconPositionsOnRow(sensors, row) = Set([beacon[1] for beacon in beaconsOnRow(sensors, row)])
+@test beaconPositionsOnRow([Sensor(0,0,0,0), Sensor(0,0,14,1), Sensor(0,0,17,0)], 0) == Set([0,17])
+@test beaconPositionsOnRow([Sensor(0,0,0,0), Sensor(0,0,14,1), Sensor(0,0,17,0)], 1) == Set([14])
 
-clearCoords(sensors, row) = symdiff(union(clearCoords.(sensors, row)...), beaconPositionsOnRow(sensors, row))
+clearCoordsOrBeacons(sensors, row) = union(clearCoords.(sensors, row)...)
 
-parseSensors(lines) = parseSensor.(lines)
+parseSensors(lines) = parseSensor.(lines) |> sensors -> sort!(sensors, by=sensor->sensor.position[1])
 
 
+tuningFrequency(gap) = gap[1]*4000000 + gap[2]
 
-part1(lines, row) = parseSensors(lines) |> sensors -> length(clearCoords(sensors, row))
+
+function findGap(sensors, maxC)
+    for row in 0:maxC
+        gaps = setdiff(0:maxC, clearCoordsOrBeacons(sensors,row))
+        if !isempty(gaps)
+            return (first(gaps),row)
+        end
+    end
+end
+
+
+part1(lines, row) = parseSensors(lines) |> sensors -> length(clearCoordsOrBeacons(sensors, row)) - length(beaconPositionsOnRow(sensors,row))
+part2(lines, maxC) = parseSensors(lines) |> sensors -> findGap(sensors, maxC) |> gap -> tuningFrequency(gap)
+
+@test 14 ∉ parseSensors(example1) |> sensors -> clearCoordsOrBeacons(sensors, 11)
 
 @test part1(example1, 10) == 26
+@test (parseSensors(example1) |> sensors -> findGap(sensors, 20)) == (14,11)
+@test part2(example1, 20) == 56000011
 
+println("Calculating...")
 @time println(part1(lines, 2000000))
+@time println(part2(lines, 4000000))

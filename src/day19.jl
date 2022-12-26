35b1c3435df9348e126ed533787efe7e5ccdca7b
Paul Mealor <paul.mealor@bjss.com>
2022-12-26 23:57:29 +0000

    Day 19 part 1 BFS

--- src/day19.jl
+++ src/day19.jl
@@ -15,6 +15,9 @@ example1 = open(readlines, "src/day19-example-1.txt")
 @enum Material ore=4 clay=3 obsidian=2 geode=1
 @show const MATERIALS = Int(typemax(Material))
 
+Base.getindex(A::AbstractArray,i::Material) = getindex(A,Int(i))
+Base.setindex!(A::AbstractArray,x,i::Material) = setindex!(A,x,Int(i))
+
 struct Robot
     type::Material
     cost::Vector{Int}
@@ -22,11 +25,12 @@ end
 function Robot(type, costs::Pair...)
     costArray = fill(0, MATERIALS)
     for cost in costs
-        costArray[Int(cost.first)] = cost.second
+        costArray[cost.first] = cost.second
     end
     return Robot(type, costArray)
 end
 Base.:(==)(a::Robot,b::Robot) = a.type == b.type && a.cost == b.cost
+Base.hash(robot::Robot) = hash(robot.type, hash(robot.cost, hash(:Robot)))
 
 struct Blueprint
     number::Int
@@ -34,6 +38,7 @@ struct Blueprint
 end
 Blueprint(number, robots...) = Blueprint(number, [robots...])
 Base.:(==)(a::Blueprint,b::Blueprint) = a.number == b.number && a.robots == b.robots
+Base.hash(b::Blueprint) = hash(b.number, hash(b.robots, hash(:Blueprint)))
 
 parseBlueprint(line) = match(r"Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.", line).captures |>
     captures -> parse.(Int, captures) |>
@@ -42,11 +47,10 @@ parseBlueprint(line) = match(r"Blueprint (\d+): Each ore robot costs (\d+) ore. 
 parseBlueprints(lines) = parseBlueprint.(lines)
 exampleBlueprints = parseBlueprints(example1)
 
-
 function asArray(robots::Vector{Pair{Material,Int}})
     result = fill(0,MATERIALS)
     for robot in robots
-        result[Int(robot.first)] = robot.second
+        result[robot.first] = robot.second
     end
     return result
 end
@@ -56,32 +60,6 @@ asArray(robots::Vector{Int}) = robots
 @test asArray([ore=>2,obsidian=>3])[Int(obsidian)] == 3
 @test asArray([ore=>2,obsidian=>3])[Int(geode)] == 0
 
-
-function heuristic(blueprint, target, robots, timeleft, materials)
-    oreRobotCost = blueprint.robots[Int(ore)].cost[Int(ore)]
-    targetRobotCost = blueprint.robots[Int(target)].cost[Int(ore)]
-
-    oreForOreRobots = materials[Int(ore)]
-    totalOreRobots = robots[Int(ore)]
-    oreForTargetRobots = materials[Int(ore)]
-    totalTargetRobots = robots[Int(target)]
-    totalTarget = materials[Int(target)]
-    for time in 1:timeleft
-        oreForOreRobots += totalOreRobots
-        newOreRobots = oreForOreRobots ÷ oreRobotCost
-        oreForOreRobots -= newOreRobots * oreRobotCost
-        totalOreRobots+=newOreRobots
-
-        totalTarget += totalTargetRobots
-        oreForTargetRobots += totalOreRobots
-        newTargetRobots = oreForTargetRobots ÷ targetRobotCost
-        oreForTargetRobots -= newTargetRobots * targetRobotCost
-        totalTargetRobots+=newTargetRobots
-    end
-
-    return totalTargetRobots
-end
-
 function maxRequired(blueprint, target)
     result = fill(0, MATERIALS)
 
@@ -89,82 +67,93 @@ function maxRequired(blueprint, target)
         result = max.(result, robot.cost)
     end
 
+    result[target] = typemax(eltype(result))
+
     return result
 end
 
-global theMostGeodes = 0
-global sample = 0
+struct State
+    robots::Vector{Int}
+    materials::Vector{Int}
+end
+Base.:(==)(a::State,b::State) = a.robots == b.robots && a.materials == b.materials
+Base.hash(state::State) = hash(state.robots, hash(state.materials, hash(:State)))
 
-@memoize Dict maxCreated(blueprint, target, robots, timeLeft) = maxCreated(blueprint, target, asArray(robots), timeLeft, fill(0,MATERIALS), maxRequired(blueprint,target), 0)
+function copyAndAdd(A, n, v)
+    R = copy(A)
+    R[n] += v
+    return R
+end
 
-global maxCreatedCache = Dict{Tuple{UInt,Material,Vector{Int},Int,Vector{Int}}, Int}()
-function maxCreated(blueprint, target, robots::Vector{Int}, timeLeft, materials, maxRobots, best)
-    return get!(maxCreatedCache, (objectid(blueprint),target,robots,timeLeft,materials)) do
+function nextStates(blueprint, target, state, maxRobots)
 
-    if timeLeft <= 0
-        #global theMostGeodes
-        #global sample
-        #if materials[Int(target)] > theMostGeodes
-        #    theMostGeodes = materials[Int(target)]
-        #    @show materials[Int(target)],robots,materials,typeof(materials)
-        #elseif sample > 100000
-        #    sample = 0
-        #    @show materials[Int(target)],robots,materials,typeof(materials)
-        #else
-        #    sample += 1
-        #end
-        return materials[Int(target)]
-    end
+    nextMaterials = state.materials .+ state.robots
+    result = State[]
 
-    result = 0
+    couldWait = false
 
-    robotCouldBeBuilt = false
-    couldBuildByWaiting = false
 
-    for robot in blueprint.robots
-        if (robots[Int(robot.type)] < maxRobots[Int(robot.type)] || robot.type==target)
-            if all(m->materials[m] >= robot.cost[m], eachindex(robot.cost))
-                robotCouldBeBuilt = true
-
-                nextMaterials = materials .+ robots .- robot.cost
-                nextRobots = copy(robots)
-                nextRobots[Int(robot.type)] += 1
-
-                result = max(result, maxCreated(blueprint, target, nextRobots, timeLeft - 1, nextMaterials, maxRobots, result))
-            elseif all(m->materials[m] + (robots[m] * (timeLeft-1)) >= robot.cost[m], eachindex(robot.cost)) #= already true: &&
-                   any(m->materials[m] < robot.cost[m], eachindex(robot.cost)) =#
-                couldBuildByWaiting = true
+    for r in blueprint.robots
+        if state.robots[r.type] < maxRobots[r.type]
+            if all(r.cost .<= state.materials)
+                push!(result, State(copyAndAdd(state.robots, r.type, 1), nextMaterials .- r.cost))
+            elseif all(state.robots .> 0 .|| r.cost .== 0)
+                couldWait = true
             end
+        elseif state.robots[target] > 0
+            couldWait = true
         end
     end
 
-    if couldBuildByWaiting
-        #println((" "^(30-timeLeft)) * "wait $(robots) $(materials) $(blueprint)")
-        result = max(result, maxCreated(blueprint, target, robots, timeLeft - 1, materials .+ robots, maxRobots, result))
-    #elseif !robotCouldBeBuilt
-    #    println((" "^(30-timeLeft)) * "nowaitnorobot $(timeLeft) $(robots) $(materials) $(blueprint)")
+    if couldWait
+        push!(result, State(state.robots,nextMaterials))
     end
 
     return result
-    end
 end
+
+triangular(n::Int) = (n^2 + n) ÷ 2
+
+function maxCreated(blueprint, target, robots, timeLeft)
+    robots = asArray(robots)
+
+    states = Set([State(robots, fill(0,MATERIALS))])
+
+    maxima = maxRequired(blueprint, target)
+
+    for t in timeLeft:-1:1
+        states = Set(flatten([nextStates(blueprint, target, state, maxima) for state in states]))
+
+        best = maximum(states) do state; state.materials[target]; end
+        if best > 0
+            filter!(states) do state
+                state.materials[target] + triangular(timeLeft-1) >= best
+            end
+        end
+
+        println("$(t) $(length(states))")
+    end
+
+    return maximum(states) do state; state.materials[target]; end
+end
+
+@show maxCreated(exampleBlueprints[1], geode, [ore=>1], 3)
+@show maxCreated(exampleBlueprints[1], geode, [ore=>1], 4)
 
 quality(blueprint::Blueprint, target, robots, timeLeft) = blueprint.number * maxCreated(blueprint, target, robots, timeLeft)
 quality(blueprints::Vector, target, robots, timeLeft) = sum([quality(blueprint, target, robots, timeLeft) for blueprint in blueprints])
 
 part1(lines) = parseBlueprints(lines) |> blueprints -> quality(blueprints, geode, [ore=>1], 24)
 
-@time @test maxCreated(exampleBlueprints[1], geode, [ore=>1], 18) == 0
-@time @test maxCreated(exampleBlueprints[1], geode, [ore=>1], 19) == 1
-@time @test maxCreated(exampleBlueprints[1], geode, [ore=>1], 24) == 9
-@time @test maxCreated(exampleBlueprints[2], geode, [ore=>1], 24) == 12
-@time @test quality(exampleBlueprints[1], geode, [ore=>1], 24) == 9
-@time @test quality(exampleBlueprints[2], geode, [ore=>1], 24) == 24
+#@time @test maxCreated(exampleBlueprints[1], geode, [ore=>1], 18) == 0
+#@time @test maxCreated(exampleBlueprints[1], geode, [ore=>1], 19) == 1
+#@time @test maxCreated(exampleBlueprints[1], geode, [ore=>1], 24) == 9
+#@time @test maxCreated(exampleBlueprints[2], geode, [ore=>1], 24) == 12
+#@time @test quality(exampleBlueprints[1], geode, [ore=>1], 24) == 9
+#@time @test quality(exampleBlueprints[2], geode, [ore=>1], 24) == 24
 @time @test quality(exampleBlueprints, geode, [ore=>1], 24) == 33
 
 @time @test part1(example1) == 33
-
-empty!(maxCreatedCache)
 
 println("Calculating...")
 @time result = part1(input)
